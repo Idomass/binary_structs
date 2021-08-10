13901cb657b86e2bce5cd4265b3d24627c4a242a
Ido Masubi <idomass@github.com>
2021-08-10 15:22:50 +0300

    Added _process_class function and __init__ auto-implementaion
    Added tests for several cases

--- unittests/test_binary_struct.py
+++ unittests/test_binary_struct.py
@@ -14,3 +14,21 @@ def test_initialization_empty_class_paranthesses():
         pass
 
     A()
+
+def test_initialization_valid_simple_class(SimpleClass):
+    a = SimpleClass(5)
+
+    assert a.a == 5
+
+def test_initialization_invalid_simple_class(SimpleClass):
+    try:
+        SimpleClass(10, 7)
+
+    except TypeError:
+        pass
+
+def test_initialization_valid_simple_class_assert_type(SimpleClass):
+    a = SimpleClass(5)
+
+    assert a.a == 5
+    assert isinstance(a.a, int)
