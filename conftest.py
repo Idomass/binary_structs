0d6a3110cb612d5be7f7fe03f8129489c2930e37
Ido Masubi <idomass@github.com>
2021-08-11 06:18:10 +0300

    Added buffer syntax
    Added buffer syntax and relevant tests.
    Also, renamed the test file

--- conftest.py
+++ conftest.py
@@ -4,10 +4,17 @@ import ctypes
 from ctypes import c_void_p
 from binary_struct import binary_struct
 
+@pytest.fixture
+def EmptyClass():
+    @binary_struct
+    class A:
+        pass
+
+    return A
 
 @pytest.fixture
 def SimpleClass():
-    @binary_struct()
+    @binary_struct
     class A:
         a: int
 
@@ -29,5 +36,14 @@ def ModuleClass():
     class A:
         ptr: c_void_p
         size: ctypes.c_uint32
+
+    return A
+
+@pytest.fixture
+def BufferClass():
+    @binary_struct
+    class A:
+        size: ctypes.c_uint32
+        buf: [ctypes.c_uint8, 32]
 
     return A
