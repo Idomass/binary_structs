78aa0690124068716b95c5a4bc668f0f44cf93cc
Ido Masubi <idomass@github.com>
2021-08-15 06:41:00 +0300

    Refactored buffer a bit
    Changed that now passed list will be used as a parameter to the underlying type

--- unittests/test_buffer.py
+++ unittests/test_buffer.py
@@ -1,3 +1,4 @@
+from ctypes import c_uint32
 from buffer import Buffer, MaxSizeExceededError
 
 
@@ -5,6 +6,9 @@ def test_valid_init():
     a = Buffer(int, 50, [1, 2, 3])
 
     assert a == [1, 2, 3]
+
+def test_valid_init_different_ctor():
+    a = Buffer(c_uint32, 5, [1, 2, 3])
 
 def test_invalid_init_type():
     try:
@@ -30,28 +34,18 @@ def test_valid_append_size():
 
     assert a == [17, 18]
 
+def test_valid_append_different_ctor():
+    a = Buffer(str, 5, [1, 2, 3])
+
+    a.append(7)
+
+    assert a == ['1', '2', '3', '7']
+
 def test_invalid_append_size():
     a = Buffer(int, 0)
 
     try:
         a.append(5)
-        assert False
-
-    except MaxSizeExceededError:
-        pass
-
-def test_valid_extend_size():
-    a = Buffer(int, 9)
-
-    a.extend(range(9))
-
-    assert a == list(range(9))
-
-def test_invalid_extend_size():
-    a = Buffer(str, 2)
-
-    try:
-        a.extend(['Noder', 'Neder', 'Nadir'])
         assert False
 
     except MaxSizeExceededError:
@@ -67,11 +61,35 @@ def test_invalid_append_type():
     except TypeError:
         pass
 
-def test_invalid_extend_type():
-    a = Buffer(str, 3)
+def test_valid_extend_size():
+    a = Buffer(int, 9)
+
+    a.extend(range(9))
+
+    assert a == list(range(9))
+
+def test_valid_extend_different_ctor():
+    a = Buffer(int, 7, [0, 1, 2, 3])
+
+    a.extend(['4', '5', '6'])
+
+    assert a == list(range(7))
+
+def test_invalid_extend_size():
+    a = Buffer(str, 2)
 
     try:
-        a.extend(['Nice', 'But', 5])
+        a.extend(['Noder', 'Neder', 'Nadir'])
+        assert False
+
+    except MaxSizeExceededError:
+        pass
+
+def test_invalid_extend_type():
+    a = Buffer(int, 3)
+
+    try:
+        a.extend([1, 'But', 5])
         assert False
 
     except TypeError:
@@ -83,6 +101,13 @@ def test_valid_insert():
     a.insert(0, 8)
 
     assert a == [8] + list(range(6))
+
+def test_valid_insert_different_ctor():
+    a = Buffer(int, 5, range(4))
+
+    a.insert(2, '99')
+
+    assert a == [0, 1, 99, 2, 3]
 
 def test_invalid_insert_size():
     a = Buffer(str, 2, ['Hi', 'There'])
@@ -111,6 +136,13 @@ def test_valid_iadd():
 
     assert a == list(range(8))
 
+def test_valid_iadd_different_ctor():
+    a = Buffer(str, 4, ['a', 'b', 'c'])
+
+    a += [5]
+
+    assert a == ['a', 'b', 'c', '5']
+
 def test_invalid_iadd_size():
     a = Buffer(int, 3, range(2))
 
@@ -122,10 +154,10 @@ def test_invalid_iadd_size():
         pass
 
 def test_invalid_iadd_type():
-    a = Buffer(str, 4, ['a', 'b', 'c'])
+    a = Buffer(int, 4, [1, 2, 3])
 
     try:
-        a += [4]
+        a += ['4a']
         assert False
 
     except TypeError:
