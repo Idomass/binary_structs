77426a700dc726efa4dbc887fc030ce1363de86b
Ido Masubi <idomass@github.com>
2021-08-28 17:28:09 +0300

    BinaryBuffer now inherits from TypedBuffer
    Changed BinaryBuffer behaviour, it will now init empty instances of
    the underlying type

--- unittests/test_binary_buffer.py
+++ unittests/test_binary_buffer.py
@@ -1,191 +1,74 @@
-from ctypes import c_uint32, c_uint8
+import pytest
+import ctypes
+
+import logging
+
 from buffers.binary_buffer import BinaryBuffer, MaxSizeExceededError
 
 
 def test_valid_init():
-    a = BinaryBuffer(int, 50, [1, 2, 3])
+    a = BinaryBuffer(ctypes.c_uint, 50, list(range(3)))
 
-    assert a == [1, 2, 3]
+    for element, expected_element in zip(a, list(range(3))):
+        assert element.value == expected_element
 
-def test_valid_init_different_ctor():
-    a = BinaryBuffer(c_uint32, 5, [1, 2, 3])
+    for element in a[3:]:
+        assert element.value == 0
 
-def test_invalid_init_type():
-    try:
-        BinaryBuffer(int, 5, ['Noder'])
-        assert False
+def test_invalid_init():
+    with pytest.raises(MaxSizeExceededError):
+        BinaryBuffer(ctypes.c_uint, 3, range(4))
 
-    except TypeError:
-        pass
+def test_invalid_append():
+    a = BinaryBuffer(ctypes.c_uint, 4)
 
-def test_invalid_init_size():
-    try:
-        BinaryBuffer(int, 3, range(4))
-        assert False
-
-    except MaxSizeExceededError:
-        pass
-
-def test_valid_append_size():
-    a = BinaryBuffer(int, 5)
-
-    a.append(17)
-    a.append(18)
-
-    assert a == [17, 18]
-
-def test_valid_append_different_ctor():
-    a = BinaryBuffer(str, 5, [1, 2, 3])
-
-    a.append(7)
-
-    assert a == ['1', '2', '3', '7']
-
-def test_invalid_append_size():
-    a = BinaryBuffer(int, 0)
-
-    try:
+    with pytest.raises(MaxSizeExceededError):
         a.append(5)
-        assert False
 
-    except MaxSizeExceededError:
-        pass
-
-def test_invalid_append_type():
-    a = BinaryBuffer(int, 5)
-
-    try:
-        a.append('Noder')
-        assert False
-
-    except TypeError:
-        pass
-
-def test_valid_extend_size():
-    a = BinaryBuffer(int, 9)
-
-    a.extend(range(9))
-
-    assert a == list(range(9))
-
-def test_valid_extend_different_ctor():
-    a = BinaryBuffer(int, 7, [0, 1, 2, 3])
-
-    a.extend(['4', '5', '6'])
-
-    assert a == list(range(7))
-
-def test_invalid_extend_size():
+def test_invalid_extend():
     a = BinaryBuffer(str, 2)
 
-    try:
+    with pytest.raises(MaxSizeExceededError):
         a.extend(['Noder', 'Neder', 'Nadir'])
-        assert False
 
-    except MaxSizeExceededError:
-        pass
-
-def test_invalid_extend_type():
-    a = BinaryBuffer(int, 3)
-
-    try:
-        a.extend([1, 'But', 5])
-        assert False
-
-    except TypeError:
-        pass
-
-def test_valid_insert():
-    a = BinaryBuffer(int, 7, range(6))
-
-    a.insert(0, 8)
-
-    assert a == [8] + list(range(6))
-
-def test_valid_insert_different_ctor():
-    a = BinaryBuffer(int, 5, range(4))
-
-    a.insert(2, '99')
-
-    assert a == [0, 1, 99, 2, 3]
-
-def test_invalid_insert_size():
+def test_invalid_insert():
     a = BinaryBuffer(str, 2, ['Hi', 'There'])
 
-    try:
+    with pytest.raises(MaxSizeExceededError):
         a.insert(1, 'Bruh')
-        assert False
 
-    except MaxSizeExceededError:
-        pass
+    with pytest.raises(MaxSizeExceededError):
+        a.insert(4, 'Bruh')
 
-def test_invalid_insert_type():
-    a = BinaryBuffer(int, 10, range(5))
+def test_invalid_iadd():
+    a = BinaryBuffer(ctypes.c_uint, 3, range(2))
 
-    try:
-        a.insert(1, 'Ahi')
-        assert False
-
-    except TypeError:
-        pass
-
-def test_valid_iadd():
-    a = BinaryBuffer(int, 10, range(5))
-
-    a += [5, 6, 7]
-
-    assert a == list(range(8))
-
-def test_valid_iadd_different_ctor():
-    a = BinaryBuffer(str, 4, ['a', 'b', 'c'])
-
-    a += [5]
-
-    assert a == ['a', 'b', 'c', '5']
-
-def test_invalid_iadd_size():
-    a = BinaryBuffer(int, 3, range(2))
-
-    try:
+    with pytest.raises(MaxSizeExceededError):
         a += [1, 2]
-        assert False
 
-    except MaxSizeExceededError:
-        pass
+def test_invalid_imul():
+    a = BinaryBuffer(ctypes.c_uint, 4, [1, 2])
 
-def test_invalid_iadd_type():
-    a = BinaryBuffer(int, 4, [1, 2, 3])
-
-    try:
-        a += ['4a']
-        assert False
-
-    except TypeError:
-        pass
-
-def test_valid_imul():
-    a = BinaryBuffer(int, 10, [1, 2])
-
-    a *= 3
-
-    assert a == [1, 2] * 3
-
-def test_invalid_imul_size():
-    a = BinaryBuffer(int, 4, [1, 2])
-
-    try:
+    with pytest.raises(MaxSizeExceededError):
         a *= 3
-        assert False
 
-    except MaxSizeExceededError:
-        pass
+def test_valid_item_assignment():
+    a = BinaryBuffer(ctypes.c_uint8, 10, [0x41] * 10)
+
+    a[0] = 0x0
+    assert a[0].value == 0
 
 def test_valid_serialization():
-    a = BinaryBuffer(c_uint8, 10, [0x41] * 10)
+    a = BinaryBuffer(ctypes.c_uint8, 10, [0x41] * 10)
 
     assert bytes(a) == b'A' * 10
 
+def test_valid_serialization_not_full():
+    a = BinaryBuffer(ctypes.c_uint8, 15, [0x41] * 10)
+
+    assert bytes(a) == b'A' * 10 + b'\x00' * 5
+
 def test_valid_serialization_empty():
-    a = BinaryBuffer(int, 0)
+    a = BinaryBuffer(ctypes.c_uint, 0)
 
     assert bytes(a) == b''
--- unittests/test_binary_struct_serialization.py
+++ unittests/test_binary_struct_serialization.py
@@ -4,7 +4,7 @@ import struct
 def test_valid_serialization(BufferClass):
     a = BufferClass(20, [0x41] * 20)
 
-    assert bytes(a) == struct.pack('I20s', 20, b'A' * 20)
+    assert bytes(a) == struct.pack('I32s', 20, b'A' * 20 + b'\x00' * 12)
 
 def test_valid_serialization_empty(EmptyClass):
     a = EmptyClass()
@@ -15,4 +15,4 @@ def test_valid_serialization_nested_class(BufferClass, NestedClass):
     a = BufferClass(20, [0x41] * 20)
     b = NestedClass(a, 0xdeadbeef)
 
-    assert bytes(b) == struct.pack('I20s', 20, b'A' * 20) + b'\xef\xbe\xad\xde'
+    assert bytes(b) == struct.pack('I32s', 20, b'A' * 20 + b'\x00' * 12) + b'\xef\xbe\xad\xde'
