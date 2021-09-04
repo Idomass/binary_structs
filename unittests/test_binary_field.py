0c98e22ff0156e2f8c9df6347e0fb94d82c1b465
Ido Masubi <idomass@github.com>
2021-09-04 18:07:31 +0300

    Added BinaryField and primitive types

---
+++ unittests/test_binary_field.py
@@ -0,0 +1,74 @@
+import pytest
+
+from utils.binary_field import uint8_t, int8_t, int16_t, uint16_t, \
+                               int32_t, uint32_t, int64_t, uint64_t, \
+                               be_uint8_t, be_int8_t, be_int16_t, be_uint16_t,\
+                               be_int32_t, be_uint32_t, be_int64_t, be_uint64_t
+
+
+# tests are catograized to:
+#   - underlying_type
+#   - default value
+#   - size
+#   - deserialization buffer
+test_buffer = [
+    # little endian
+    (int8_t, -128, 1, b'\x80'),
+    (uint8_t, 250, 1, b'\xfa'),
+    (int16_t, -11214, 2, b'\x32\xd4'),
+    (uint16_t, 65001, 2, b'\xe9\xfd'),
+    (int32_t, -12891289, 4, b'\x67\x4b\x3b\xff'),
+    (uint32_t, 9384012, 4, b'\x4c\x30\x8f\x00'),
+    (int64_t, -3712379149898, 8, b'\xff\xff\xfc\x9f\xa4\xf5\xa1\xb6'[::-1]),
+    (uint64_t, 28427847382434, 8, b'\x00\x00\x19\xda\xdf\xbe\xb5\xa2'[::-1]),
+
+    # big endian
+    (be_int8_t, -128, 1, b'\x80'),
+    (be_uint8_t, 250, 1, b'\xfa'),
+    (be_int16_t, -11214, 2, b'\x32\xd4'[::-1]),
+    (be_uint16_t, 65001, 2, b'\xe9\xfd'[::-1]),
+    (be_int32_t, -12891289, 4, b'\x67\x4b\x3b\xff'[::-1]),
+    (be_uint32_t, 9384012, 4, b'\x4c\x30\x8f\x00'[::-1]),
+    (be_int64_t, -3712379149898, 8, b'\xff\xff\xfc\x9f\xa4\xf5\xa1\xb6'),
+    (be_uint64_t, 28427847382434, 8, b'\x00\x00\x19\xda\xdf\xbe\xb5\xa2'),
+]
+
+
+@pytest.mark.parametrize('underlying_type, default_value, size, buf', test_buffer)
+def test_valid_init(underlying_type, default_value, size, buf):
+    a = underlying_type(default_value)
+
+    assert a == default_value
+
+@pytest.mark.parametrize('underlying_type, default_value, size, buf', test_buffer)
+def test_valid_size(underlying_type, default_value, size, buf):
+    a = underlying_type(default_value)
+
+    assert a.size == size
+
+@pytest.mark.parametrize('underlying_type, default_value, size, buf', test_buffer)
+def test_valid_deserialization(underlying_type, default_value, size, buf):
+    a = underlying_type()
+
+    a.deserialize(buf)
+    assert a == default_value
+
+@pytest.mark.parametrize('underlying_type, default_value, size, buf', test_buffer)
+def test_invalid_deserialization_empty(underlying_type, default_value, size, buf):
+    a = underlying_type()
+
+    with pytest.raises(ValueError):
+        a.deserialize(b'')
+
+@pytest.mark.parametrize('underlying_type, default_value, size, buf', test_buffer)
+def test_valid_serialization(underlying_type, default_value, size, buf):
+    a = underlying_type(default_value)
+
+    assert bytes(a) == buf
+
+
+@pytest.mark.parametrize('underlying_type, default_value, size, buf', test_buffer)
+def test_valid_serialization_empty(underlying_type, default_value, size, buf):
+    a = underlying_type()
+
+    assert bytes(a) == b'\x00' * a.size
---
+++ utils/binary_field.py
@@ -0,0 +1,123 @@
+"""
+Binary field is used by binary structs.
+Each field in binary structs must implement:
+    - serialization
+    - deserialization
+    - size property
+    - endianness for ? types
+
+Binary field enforces that each class implements these things.
+1"""
+
+import struct
+import ctypes
+
+from abc import abstractmethod
+from typing import List
+
+
+class BinaryField:
+    @classmethod
+    @abstractmethod
+    def deserialize(self, buffer):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def __bytes__(self):
+        pass
+
+    @property
+    @abstractmethod
+    def size(self):
+        pass
+
+class PrimitiveTypeField(BinaryField):
+    """
+    Designed for primitive ctypes types
+    """
+
+    def deserialize(self, buffer):
+        if len(buffer) < self.size:
+            raise ValueError('Given buffer is too small!')
+
+        self.__init__(struct.unpack(self.FORMAT, buffer)[0])
+
+    @property
+    def size(self):
+        return ctypes.sizeof(self)
+
+    def __eq__(self, number) -> bool:
+        if isinstance(number, int):
+            return self.value == number
+
+        return super().__eq__(number)
+
+    def __bytes__(self) -> bytes:
+        return struct.pack(self.FORMAT, self.value)
+
+class int8_t(ctypes.c_int8, PrimitiveTypeField):
+    FORMAT = 'b'
+
+class uint8_t(ctypes.c_uint8, PrimitiveTypeField):
+    FORMAT = 'B'
+
+class int16_t(ctypes.c_int16, PrimitiveTypeField):
+    FORMAT = 'h'
+
+class uint16_t(ctypes.c_uint16, PrimitiveTypeField):
+    FORMAT = 'H'
+
+class int32_t(ctypes.c_int32, PrimitiveTypeField):
+    FORMAT = 'i'
+
+class uint32_t(ctypes.c_uint32, PrimitiveTypeField):
+    FORMAT = 'I'
+
+class int64_t(ctypes.c_int64, PrimitiveTypeField):
+    FORMAT = 'q'
+
+class uint64_t(ctypes.c_uint64, PrimitiveTypeField):
+    FORMAT = 'Q'
+
+def big_endian(cls=None):
+    def wrap(cls):
+        cls.FORMAT = f'>{cls.FORMAT}'
+        return cls
+
+    if cls is None:
+        return wrap
+
+    return wrap(cls)
+
+@big_endian
+class be_int8_t(int8_t):
+    pass
+
+@big_endian
+class be_uint8_t(uint8_t):
+    pass
+
+@big_endian
+class be_int16_t(int16_t):
+    pass
+
+@big_endian
+class be_uint16_t(uint16_t):
+    pass
+
+@big_endian
+class be_int32_t(int32_t):
+    pass
+
+@big_endian
+class be_uint32_t(uint32_t):
+    pass
+
+@big_endian
+class be_int64_t(int64_t):
+    pass
+
+@big_endian
+class be_uint64_t(uint64_t):
+    pass
