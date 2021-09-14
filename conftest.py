9da7081b77587655d584527e2005d61cf5fe361b
Ido Masubi <idomass@github.com>
2021-09-14 14:24:13 +0300

    Updated __eq__ tests
    They now use paramerterize as well
    Moved available_struct to the conftest file

--- conftest.py
+++ conftest.py
@@ -4,6 +4,9 @@ from utils.buffers.typed_buffer import TypedBuffer
 from utils.binary_field import uint8_t, uint32_t
 from binary_struct import binary_struct, _process_class
 
+# misc
+def empty_decorator(cls):
+    return cls
 
 # buffers
 @pytest.fixture
@@ -53,6 +56,43 @@ class InheritedClass(BufferClass):
 @binary_struct
 class MultipleInheritedClass(BufferClass, SimpleClass, SomeBaseClass):
     magic: uint32_t
+
+# List for parameterize tests
+test_structs = [
+    [
+        EmptyClass,
+        {}
+    ],
+    [
+        SimpleClass,
+        {'a': 89}
+    ],
+    [
+        BufferClass,
+        {'size': 423652, 'buf': range(3)}
+    ],
+    [
+        DuplicateClass,
+        {'magic': 0xcafebabe}
+    ],
+    [
+        DynamicClass,
+        {'magic': 0xef, 'buf': range(99)}
+    ],
+    [
+        InheritedClass,
+        {'size': 50, 'buf': range(9), 'magic': 0x12345678}
+    ],
+    [
+        MultipleInheritedClass,
+        {'size': 99, 'buf': range(15), 'a': 7 ,'magic': 0x90807060}
+    ],
+
+    [
+        NestedClass,
+        {'buffer': [5, [1]], 'magic': 0xdeadbeef}
+    ]
+]
 
 # Fixtures
 @pytest.fixture
